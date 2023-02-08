09e2fd03622d570fbb8545d01c8f4b8e6ea56ea7
Roman Glukhenko <grn-it@yandex.ru>
2023-02-08 12:28:50 +0400

    fix: added no cache for curl

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -s -H 'Cache-Control: no-cache, no-store' https://raw.githubusercontent.com/grn-it/symfony-web-application-install-way/main/Dockerfile -o Dockerfile
-curl -s -H 'Cache-Control: no-cache, no-store' https://raw.githubusercontent.com/grn-it/symfony-web-application-install-way/main/docker-compose.dev.yml -o docker-compose.dev.yml
-curl -s -H 'Cache-Control: no-cache, no-store' https://raw.githubusercontent.com/grn-it/symfony-web-application-install-way/main/Makefile -o Makefile
+curl -s "https://raw.githubusercontent.com/grn-it/symfony-web-application-install-way/main/Dockerfile?$(date +%s)" -o Dockerfile
+curl -s "https://raw.githubusercontent.com/grn-it/symfony-web-application-install-way/main/docker-compose.dev.yml?$(date +%s)" -o docker-compose.dev.yml
+curl -s "https://raw.githubusercontent.com/grn-it/symfony-web-application-install-way/main/Makefile?$(date +%s)" -o Makefile
 make install
