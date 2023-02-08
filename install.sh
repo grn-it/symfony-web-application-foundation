62f7bf5a768d1d73bd53f8724098bff99b9b62d8
Roman Glukhenko <grn-it@yandex.ru>
2023-02-08 09:57:26 +0400

    feat: added install.sh for installing with curl

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+curl -s https://raw.githubusercontent.com/grn-it/symfony-web-application-install-way/main/Dockerfile -o Dockerfile
+curl -s https://raw.githubusercontent.com/grn-it/symfony-web-application-install-way/main/docker-compose.dev.yml -o docker-compose.dev.yml
+curl -s https://raw.githubusercontent.com/grn-it/symfony-web-application-install-way/main/Makefile -o Makefile
